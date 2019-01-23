33ba6e6044d838e0769d64cd51c978f5d69ff9b1
mastah7991 <mastah7991@gmail.com>
2019-01-23 23:18:13 +0100

    Update Orders.sql

--- Orders.sql
+++ Orders.sql
@@ -1,1 +1,5 @@
 
+CREATE PROCEDURE InitOrder(@person INT, @Cauntry NVarchar(30), @Address NVarchar(30),@City NVarchar(30))
+AS
+	INSERT INTO Orders VALUES(GETDATE(),@City,@Cauntry,@Address,(SELECT Login FROM Clients WHERE @person = PeopleId));
+GO
