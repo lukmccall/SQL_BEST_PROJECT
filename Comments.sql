9b5e56b2c09c2152e2f490e36b865576ba05ccb3
mastah7991 <mastah7991@gmail.com>
2019-01-23 22:10:00 +0100

    Create Comments.sql

---
+++ Comments.sql
@@ -0,0 +1,9 @@
+
+INSERT INTO Comments VALUES(1,'meme',GETDATE(),'hehe'),
+(1,'wzor',GETDATE()+1,'hehe XD'),
+(1,'Wskaznik',GETDATE()+2,'jasne jasne'),
+(1,'meme',GETDATE()+3,'a czemu nie?'),
+(1,'Wskaznik',GETDATE()+4,'a ma pan dowodzik?'),
+(1,'wzor',GETDATE()+5,'slabo :/'),
+(2,'meme',GETDATE()+6,'jupi!'),
+(2,'meme',GETDATE()+7,'To najlepsze co mnie dzis spotkalo!')
