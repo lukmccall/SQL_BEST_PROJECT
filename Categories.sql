9c80797d2de97e46d65330406de7a18ce6def7fe
LukMcCall <luk1555@wp.pl>
2019-01-23 23:37:19 +0100

    Kolejne generatory - kategorie i produkty

--- Categories.sql
+++ Categories.sql
@@ -6,4 +6,6 @@ INSERT INTO Categories(Category) VALUES
 (N'Artyku³y budowlane'),
 (N'Meble'),
 (N'Ubrania'),
-(N'Kursy')
+(N'Kursy'),
+(N'Internet')
+
---
+++ Products.sql
@@ -0,0 +1,9 @@
+EXEC dbo.uspAddItem 'Biurko', '£adne biurko', 'Biuro'
+EXEC dbo.uspAddItem '£adowarka', 'Danielowi siê przyda', 'Elektronika'
+EXEC dbo.uspAddItem 'Musztarda', 'O nie, najgorzej', 'Chemia'
+EXEC dbo.uspAddItem 'Cpp2000', 'Najnowszy kurs cpp', 'Kursy'
+
+EXEC dbo.uspAddService 'Sprz¹tanie', 1, 'No przyjdziemy i posprz¹tamy', 'Dom'
+EXEC dbo.uspAddService 'Konto Netflix', 1, 'Netflix And Chill', 'Internet'
+EXEC dbo.uspAddService 'Serwer nibus2k', 1, 'Szybkie to', 'Internet'
+EXEC dbo.uspAddService 'Tankowanie do pe³na', 1, 'No przyjdziemy i pojedziesz', 'Chemia'
