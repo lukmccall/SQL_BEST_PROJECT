10553f16d14ac9f23132a053858850483e44b559
Tomasz Janik <41924241+tomasz-janik@users.noreply.github.com>
2019-01-23 23:55:52 +0100

    widokow 6 coraz wiecej

--- Widoki_Funkcje.sql
+++ Widoki_Funkcje.sql
@@ -46,3 +46,41 @@ BEGIN
 	RETURN
 END
 GO
+
+!--funkcyja liczy ile miesiecy minelo bo potrzebuje do nastepnej funkcji (super zastosowanie tomek polecam)
+CREATE FUNCTION FullMonthsSeparation 
+(
+    @DateA DATETIME,
+    @DateB DATETIME
+)
+RETURNS INT
+AS
+BEGIN
+    DECLARE @Result INT
+
+    DECLARE @DateX DATETIME
+    DECLARE @DateY DATETIME
+
+    IF(@DateA < @DateB)
+    BEGIN
+        SET @DateX = @DateA
+        SET @DateY = @DateB
+    END
+    ELSE
+    BEGIN
+        SET @DateX = @DateB
+        SET @DateY = @DateA
+    END
+
+    SET @Result = (
+                    SELECT 
+                    CASE 
+                        WHEN DATEPART(DAY, @DateX) > DATEPART(DAY, @DateY)
+                        THEN DATEDIFF(MONTH, @DateX, @DateY) - 1
+                        ELSE DATEDIFF(MONTH, @DateX, @DateY)
+                    END
+                    )
+
+    RETURN @Result
+END
+GO
