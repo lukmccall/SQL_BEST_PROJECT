5a2490bb8eba443b951c14e9865bc3a667e61498
Tomasz Janik <41924241+tomasz-janik@users.noreply.github.com>
2019-01-23 22:36:22 +0100

    3 viewy

--- View_Top5.sql
+++ View_Top5.sql
@@ -11,3 +11,13 @@ AS
 	SELECT TOP 5 CommentBody FROM dbo.Comments
 	ORDER BY AddDate DESC
 GO
+
+//bardzo ładny tutaj view jak mnie uczyli na zajeciach mi sie podoba nie zadna funkcja
+CREATE VIEW itemsToSend
+AS
+SELECT O.Id, O.Country, O.City, O.Address, OD.ProductsId, OD.Quantity FROM Status AS S JOIN OrdersStatus AS OS
+ON S.Id = OS.StatusId  JOIN Orders AS O
+ON OS.OrdersId = O.Id JOIN OrdersDetails AS OD
+ON O.Id = OD.OrdersId
+WHERE S.StatusInfo LIKE N'TO SEND' 
+GO
