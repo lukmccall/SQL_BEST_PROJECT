243b37d00e69f1e0b8822fc771c43b0f106b3f19
LukMcCall <luk1555@wp.pl>
2019-01-23 23:25:39 +0100

    Generatory dla kategori i magazynów

---
+++ Categories.sql
@@ -0,0 +1,9 @@
+INSERT INTO Categories(Category) VALUES
+(N'Dom'),
+(N'Biuro'),
+(N'Elektronika'),
+(N'Chemia'),
+(N'Artyku³y budowlane'),
+(N'Meble'),
+(N'Ubrania'),
+(N'Kursy')
---
+++ Warehouse.sql
@@ -0,0 +1,4 @@
+INSERT INTO Warehouse(Name, Country, City, Address, Phone) VALUES 
+('G³ówny', 'Poland', 'Bielsko-Bia³a', 'Malinowa 14a', 123456789),
+('Dodatkowy', 'Poland', 'Sosnowiec', 'Leœna 21', 654123987),
+('Rezerwowy', 'Poland', 'Kraków', 'Ulica 123', 123456789)
