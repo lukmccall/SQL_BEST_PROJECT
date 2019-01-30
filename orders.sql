f7b275598261551b3598fe092fec8d9aa7b18c0b
mastah7991 <mastah7991@gmail.com>
2019-01-30 13:50:28 +0100

    gupi trigger

---
+++ orders.sql
@@ -0,0 +1,58 @@
+
+CREATE PROCEDURE InitOrder(@person INT, @Cauntry NVarchar(30), @Address NVarchar(30),@City NVarchar(30))
+AS
+	INSERT INTO Orders VALUES(GETDATE(),@City,@Cauntry,@Address,(SELECT Login FROM Clients WHERE @person = PeopleId));
+GO
+
+								     
+								     
+CREATE TRIGGER OrderDetailsGetPrice ON OrdersDetails
+INSTEAD OF INSERT
+AS
+
+BEGIN TRY
+	DECLARE @ProductId INT
+	DECLARE @OrderId INT
+	DECLARE @Discount INT
+	DECLARE @Quantity INT
+
+	DECLARE K_NextInser CURSOR 
+	FOR SELECT OrdersId, ProductsId, Discount, Quantity FROM inserted 
+	FOR READ ONLY
+		
+	Open K_NextInser 
+	FETCH K_NextInser INTO @OrderId, @ProductId, @Discount, @Quantity
+			
+	WHILE (@@FETCH_STATUS<>-1)
+	BEGIN
+		DECLARE @Login nVARCHAR(30) = (SELECT ClientsLogin FROM Orders WHERE Id = @OrderId)
+
+		DECLARE @result int;  
+		EXECUTE @result = dbo.uspSellItem @ProductId, NULL, @Quantity;  
+
+
+		IF (@result = 1 AND @result IS NOT NULL)
+		BEGIN 
+		
+			--EXEC dbo.uspActiveCode @CodeId, @Login, @Discount OUT
+
+			DECLARE @UnitPrice MONEY = dbo.GetPrice(@ProductId,GETDATE())
+			INSERT INTO OrdersDetails VALUES(@OrderId,@ProductId,@UnitPrice, @Quantity,@Discount)
+			
+		END
+		ELSE
+		BEGIN
+			RAISERROR(50002,-1,-1) 
+		END	
+
+ 		
+		FETCH K_NextInser INTO @OrderId, @ProductId, @Discount, @Quantity
+	END
+	CLOSE K_NextInser 
+	DEALLOCATE K_NextInser 
+
+END TRY
+BEGIN CATCH
+	EXEC dbo.uspDisplayErrors
+END CATCH
+GO
--- ordersProc.sql
+++
@@ -1,5 +0,0 @@
-
-CREATE PROCEDURE InitOrder(@person INT, @Cauntry NVarchar(30), @Address NVarchar(30),@City NVarchar(30))
-AS
-	INSERT INTO Orders VALUES(GETDATE(),@City,@Cauntry,@Address,(SELECT Login FROM Clients WHERE @person = PeopleId));
-GO
